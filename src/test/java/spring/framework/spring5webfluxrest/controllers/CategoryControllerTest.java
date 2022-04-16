97197fcb265ea348b868207534b07ff5d933b33c
Pattriarch <pgelo298@mail.ru>
2022-04-16 20:04:00 +0300

    Added POST + PUT methods for categories

--- src/test/java/spring/framework/spring5webfluxrest/controllers/CategoryControllerTest.java
+++ src/test/java/spring/framework/spring5webfluxrest/controllers/CategoryControllerTest.java
@@ -4,13 +4,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
 import org.mockito.Mockito;
+import org.reactivestreams.Publisher;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import spring.framework.spring5webfluxrest.domain.Category;
 import spring.framework.spring5webfluxrest.repositories.CategoryRepository;
 
-import static org.hamcrest.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CategoryControllerTest {
@@ -43,8 +44,38 @@ class CategoryControllerTest {
         BDDMockito.given(categoryRepository.findById("someId"))
                 .willReturn(Mono.just(Category.builder().description("Cat1").build()));
 
-        webTestClient.get().uri("/api/v1/categories/someid")
+        webTestClient.get().uri("/api/v1/categories/someId")
                 .exchange()
                 .expectBody(Category.class);
+    }
+
+    @Test
+    void testCreateCategory() {
+        BDDMockito.given(categoryRepository.saveAll(any(Publisher.class)))
+                .willReturn(Flux.just(Category.builder().build()));
+
+        Mono<Category> catToSaveMono = Mono.just(Category.builder().description("Some info").build());
+
+        webTestClient.post()
+                .uri("/api/v1/categories")
+                .body(catToSaveMono, Category.class)
+                .exchange()
+                .expectStatus()
+                .isCreated();
+    }
+
+    @Test
+    public void testUpdate() {
+        BDDMockito.given(categoryRepository.save(any(Category.class)))
+                .willReturn(Mono.just(Category.builder().build()));
+
+        Mono<Category> catToUpdate = Mono.just(Category.builder().description("Some info").build());
+
+        webTestClient.put()
+                .uri("/api/v1/categories")
+                .body(catToUpdate, Category.class)
+                .exchange()
+                .expectStatus()
+                .isOk();
     }
 }
